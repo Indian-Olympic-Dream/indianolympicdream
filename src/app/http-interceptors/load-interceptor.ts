e816d03a5e4135aec82daf68c40fa83cb9932544
Ankush Agarwal <96ankush@gmail.com>
2026-04-18 20:35:25 +0530

    feat: redirect to internal-error page on network or gateway timeout errors in load interceptor

--- src/app/http-interceptors/load-interceptor.ts
+++ src/app/http-interceptors/load-interceptor.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpSentEvent, HttpHeaderResponse, HttpProgressEvent, HttpResponse, HttpUserEvent } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { LoaderService } from '../shared/components/loader/loader.service';
 @Injectable()
 export class LoadInterceptor implements HttpInterceptor {
+  private router = inject(Router);
   constructor(
     private loaderService: LoaderService,
   ) { }
@@ -27,6 +29,10 @@ export class LoadInterceptor implements HttpInterceptor {
             statusText: error.statusText,
             message: error.message,
           });
+          // 502: Bad Gateway, 503: Service Unavailable, 504: Gateway Timeout
+          if (error.status === 502 || error.status === 503 || error.status === 504 || error.status === 0) {
+            this.router.navigate(['internal-error']);
+          }
         }
         return throwError(() => error);
       }),
@@ -36,11 +42,9 @@ export class LoadInterceptor implements HttpInterceptor {
     this.hideLoader();
   }
   private showLoader(): void {
-    // console.log('loader show');
     this.loaderService.show();
   }
   private hideLoader(): void {
-    // console.log('loader hide');
     this.loaderService.hide();
   }
 }
